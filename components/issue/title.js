113f03289723abf274ff75d9d3a53c59978cb345
wangsijie <i@sijie.wang>
2020-03-18 18:44:30 +0800

    issue time

--- components/issue/title.js
+++ components/issue/title.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Badge } from 'antd';
 import { DeleteOutlined } from '@ant-design/icons';
 import axios from 'axios';
+import moment from 'moment';
 import './style.less';
 
 const defaultCategory = { title: 'other', color: '#fff', fontColor: '#000' };
@@ -17,6 +18,7 @@ export default function IssueTitle({ issue, number, onDeleted, categories }) {
         <div className="title">
             #{number} {issue.title}
             {issue.content && <div className="note"><Badge count="笔记" style={{ backgroundColor: '#d9d9d9', color: '#000' }} /></div>}
+            <div className="time">{moment(issue.createdAt).format('HH:mm')}</div>
         </div>
         <div className="remove" onClick={handleDelete}><DeleteOutlined /></div>
         <div className="category"><Badge count={category.title} style={{ backgroundColor: category.color, color: category.fontColor }} /></div>
