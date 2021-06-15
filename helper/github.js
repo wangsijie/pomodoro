fcc25110ca9de7b56248e7af6ba4be3d1bb44ad3
wangsijie <i@sijie.wang>
2021-06-15 13:38:29 +0800

    github proxy

--- helper/github.js
+++ helper/github.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 export const CLIENT_ID = process.env.GH_ID;
 export const CLIENT_SECRET = process.env.GH_SK;
+const GITHUB_BASE_URL = process.env.GH_BASE_URL || 'https://github.com';
+const GITHUB_API_URL = process.env.GH_API_URL || 'https://api.github.com' ;
 
 export const requestAccessToken = async (code) => {
     const res = await axios.post(
-        'https://github.com/login/oauth/access_token',
+        `${GITHUB_BASE_URL}/login/oauth/access_token`,
         {
             client_id: CLIENT_ID,
             client_secret: CLIENT_SECRET,
@@ -21,6 +23,6 @@ export const requestAccessToken = async (code) => {
 }
 
 export const getUserInfo = async (token) => {
-    const res = await axios.get('https://api.github.com/user', { headers: { authorization: `bearer ${token}` } });
+    const res = await axios.get(`${GITHUB_API_URL}/user`, { headers: { authorization: `bearer ${token}` } });
     return res.data;
 }
